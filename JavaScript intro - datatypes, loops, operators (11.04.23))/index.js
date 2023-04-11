da8fdd90ac0d1e0e982527c17baefcd8e5b0c7a7
Alijan Mirzayev <mirzeyevalican1@gmail.com>
2023-04-11 18:30:57 +0400

    added #1 task

---
+++ JavaScript intro - datatypes, loops, operators (11.04.23))/index.js
@@ -0,0 +1,11 @@
+// 1. Bir dəyişkən təyin edin və ona bir ədəd mənimsədin. Ədədin mənfi, müsbət və ya sıfır olub olmadığını yoxlayın və cavabı console-da göstərin.
+
+let number = -1
+
+if (number > 0) {
+    console.log(`${number} müsbət ədəddir`)
+} else if (number < 0) {
+    console.log(`${number} mənfi ədəddir`)
+} else {
+    console.log(`${number} - Sıfır`)
+}
